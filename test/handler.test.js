d4ef812f229f8401c5c9273ece3a964ed44cf1ba
antklim <antklim@gmail.com>
2017-11-06 09:55:45 +1100

    Unit tests added

--- test/handler.test.js
+++ test/handler.test.js
@@ -14,7 +14,30 @@ test('main() should return correct status and error when passed unsupported meth
   t.deepEqual(error, {httpStatus: 500, error: 'Method \'OPTION\' not supported'})
 })
 
-test.todo('main() should invoke readResource when method is GET')
-test.todo('main() should invoke updateResource when method is PUT')
-test.todo('main() should invoke createResource when method is POST')
-test.todo('main() should invoke deleteResource when method is DELETE')
+test('main() should invoke readResource when method is GET', t => {
+  const readResource = td.replace(handler, 'readResource')
+  handler.main({method: 'GET'}, {})
+  td.verify(readResource({method: 'GET'}, {}))
+  t.pass()
+})
+
+test('main() should invoke updateResource when method is PUT', t => {
+  const updateResource = td.replace(handler, 'updateResource')
+  handler.main({method: 'PUT'}, {})
+  td.verify(updateResource({method: 'PUT'}, {}))
+  t.pass()
+})
+
+test('main() should invoke createResource when method is POST', t => {
+  const createResource = td.replace(handler, 'createResource')
+  handler.main({method: 'POST'}, {})
+  td.verify(createResource({method: 'POST'}, {}))
+  t.pass()
+})
+
+test('main() should invoke deleteResource when method is DELETE', t => {
+  const deleteResource = td.replace(handler, 'deleteResource')
+  handler.main({method: 'DELETE'}, {})
+  td.verify(deleteResource({method: 'DELETE'}, {}))
+  t.pass()
+})
